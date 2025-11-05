12a332eaf2746b28892e2a678810c4accfde0ac2
Max <maxledlie@outlook.com>
2025-11-05 18:26:53 +0000

    Draw bodies of lasers

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,7 @@ const state = {
     debug: false,
     placementStart: null,
     editMode: "laser",
-    rays: [],
-    mirrors: []
+    entities: [],
 };
 function p5_setup(p) {
     p.createCanvas(p.windowWidth, p.windowHeight);
@@ -16,14 +15,40 @@ function p5_draw(p) {
     p.noStroke();
     p.text(state.editMode == "laser" ? "> draw laser (L)" : "  draw laser (L)", 10, 20);
     p.text(state.editMode == "mirror" ? "> draw mirror (M)" : "  draw mirror (M)", 10, 40);
+    // Draw entities
+    for (const entity of state.entities) {
+        switch (entity.type) {
+            case "laser":
+                drawLaser(p, entity);
+                break;
+        }
+    }
+    const lasers = state.entities.filter(e => e.type == "laser");
+    const mirrors = state.entities.filter(e => e.type == "mirror");
+    // Work out the segments to actually draw
+    const segments = [];
+    for (const laser of lasers) {
+        let tmin = Infinity;
+        for (const mirror of mirrors) {
+            const t = rayIntersectSegment(laser, mirror);
+            if (t != null && t >= 0 && t < tmin) {
+                tmin = t;
+            }
+        }
+        if (tmin == Infinity) {
+            // No intersection: Find end point of line very far along the direction from mouse start to mouse end
+            segments.push({ start: laser.start, end: pointOnRay(laser, 10000) });
+        }
+        else {
+            segments.push({ start: laser.start, end: pointOnRay(laser, tmin) });
+        }
+    }
     p.stroke("yellow");
-    for (const ray of state.rays) {
-        // Find end point of line very far along the direction from mouse start to mouse end
-        const endPoint = vec_add(ray.start, vec_mul(ray.direction, 10000));
-        p.line(ray.start.x, ray.start.y, endPoint.x, endPoint.y);
+    for (const segment of segments) {
+        p.line(segment.start.x, segment.start.y, segment.end.x, segment.end.y);
     }
     p.stroke("lightblue");
-    for (const { start, end } of state.mirrors) {
+    for (const { start, end } of mirrors) {
         p.line(start.x, start.y, end.x, end.y);
     }
     if (state.placementStart) {
@@ -39,6 +64,19 @@ function p5_draw(p) {
             p.line(state.placementStart.x, state.placementStart.y, p.mouseX, p.mouseY);
         }
     }
+}
+function drawLaser(p, laser) {
+    // Draw rectangle behind the starting point representing the laser generator
+    // Calculate angle of beam
+    const theta = Math.atan2(laser.direction.y, laser.direction.x);
+    p.push();
+    p.translate(laser.start.x, laser.start.y);
+    p.rotate(theta);
+    p.fill("yellow");
+    p.circle(0, 0, 5);
+    p.fill("white");
+    p.rect(-40, -10, 40, 20);
+    p.pop();
 }
 function drawnRay(p) {
     const mouse = { x: p.mouseX, y: p.mouseY };
@@ -63,10 +101,10 @@ function p5_mouse_released(p) {
     if (state.placementStart) {
         if (state.editMode == "laser") {
             const ray = drawnRay(p);
-            state.rays.push(ray);
+            state.entities.push({ type: "laser", start: ray.start, direction: ray.direction });
         }
         else if (state.editMode == "mirror") {
-            state.mirrors.push({ start: state.placementStart, end: { x: p.mouseX, y: p.mouseY } });
+            state.entities.push({ type: "mirror", start: state.placementStart, end: { x: p.mouseX, y: p.mouseY } });
         }
     }
     state.placementStart = null;
@@ -81,14 +119,14 @@ const s = (p) => {
     p.touchEnded = () => p5_mouse_released(p);
 };
 const sketch = new p5(s);
-function rayIntersectEdge(ray, edge, nodes) {
+function rayIntersectSegment(ray, segment) {
     // Given a ray and a line segment defined by its start and end,
     // returns the t-value of intersection if it exists, else null.
     // The t-value is returned even if negative.
     const x1 = ray.start;
-    const x2 = nodes[edge.from].point;
+    const x2 = segment.start;
     const d1 = vec_normalize(ray.direction);
-    const d2 = vec_sub(nodes[edge.to].point, nodes[edge.from].point);
+    const d2 = vec_sub(segment.end, segment.start);
     const length2 = vec_magnitude(d2);
     const v1 = d1;
     const v2 = vec_div(d2, length2);
