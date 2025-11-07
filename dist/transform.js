7d37822034f161ff4eebabcc06c59d8bb2b91a9c
Max <maxledlie@outlook.com>
2025-11-07 17:11:54 +0100

    Add Transform class for objects. This doesn't really do what we need for camera transforms though.

---
+++ dist/transform.js
@@ -0,0 +1,37 @@
+import { mat3_chain, mat3_inverse, mat3_mul_vec, rotation, translation, scale } from "./math";
+export default class Transform {
+    constructor() {
+        this._rotation = 0;
+        this._scale = { x: 1, y: 1 };
+        this._translation = { x: 0, y: 0 };
+    }
+    rotate(theta) {
+        this._rotation += theta;
+    }
+    scale(x, y) {
+        const _y = y !== null && y !== void 0 ? y : x;
+        this._scale.x *= x;
+        this._scale.y *= _y;
+    }
+    translate(x, y) {
+        this._translation.x += x;
+        this._translation.y += y;
+    }
+    apply(v) {
+        const mat = mat3_chain([
+            translation(this._translation.x, this._translation.y),
+            rotation(this._rotation),
+            scale(this._scale.x, this._scale.y)
+        ]);
+        return mat3_mul_vec(mat, v);
+    }
+    applyInverse(v) {
+        const mat = mat3_chain([
+            translation(this._translation.x, this._translation.y),
+            rotation(this._rotation),
+            scale(this._scale.x, this._scale.y)
+        ]);
+        const inv = mat3_inverse(mat);
+        return mat3_mul_vec(inv, v);
+    }
+}
---
+++ transform.ts
@@ -0,0 +1,41 @@
+import { mat3_chain, mat3_inverse, mat3_mul_vec, rotation, translation, scale, Vector } from "./math";
+
+export default class Transform {
+    _rotation: number = 0;
+    _scale: Vector = { x: 1, y: 1 };
+    _translation: Vector = { x: 0, y: 0 };
+
+    rotate(theta: number) {
+        this._rotation += theta;
+    }
+
+    scale(x: number, y?: number) {
+        const _y = y ?? x;
+        this._scale.x *= x;
+        this._scale.y *= _y;
+    }
+
+    translate(x: number, y: number) {
+        this._translation.x += x;
+        this._translation.y += y;
+    }
+
+    apply(v: Vector): Vector {
+        const mat = mat3_chain([
+            translation(this._translation.x, this._translation.y),
+            rotation(this._rotation),
+            scale(this._scale.x, this._scale.y)
+        ]);
+        return mat3_mul_vec(mat, v);
+    }
+
+    applyInverse(v: Vector): Vector {
+        const mat = mat3_chain([
+            translation(this._translation.x, this._translation.y),
+            rotation(this._rotation),
+            scale(this._scale.x, this._scale.y)
+        ]);
+        const inv = mat3_inverse(mat);
+        return mat3_mul_vec(inv, v);
+    }
+}
